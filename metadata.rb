c3f1b9e60d58559f8e6a55ffe3d74af161870ef0
David Hofmann <david.hofmann@am.sony.com>
2015-04-16 14:05:14 -0400

    Adjust Version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'elmic11111@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures yum-updatesd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'redhat'
 supports 'centos'
